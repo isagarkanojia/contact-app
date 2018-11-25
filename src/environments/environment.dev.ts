37aa03d4e6e27786870e58d0458b2291e9e561fe
Sagar Kanojia <sagar.k@mindfiresolutions.com>
2018-11-25 16:48:52 +0530

    Change API url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,14 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: "http://localhost:9400",
+  apiBaseUrl: "https://intense-brook-76978.herokuapp.com",
   version: '1.0.0',
 };
 
-// export const checkMode = {
-//   mode: (environment.apiBaseUrl.indexOf("dev") > 0) ? "dev" :
-//     (environment.apiBaseUrl.indexOf("uat") > 0) ? "uat" : "prod",
-//     sso: (environment.apiBaseUrl.indexOf("dev") > 0) ? "https://myreports.dev.phototype.com" :
-//       (environment.apiBaseUrl.indexOf("uat") > 0) ? "https://myreports.uat.phototype.com" :
-//         (environment.apiBaseUrl.indexOf("local") > 0) ? "http://localhost:4400" :
-//           "https://myreports.phototype.com"
-// }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,11 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:9400',
+  apiBaseUrl: 'https://intense-brook-76978.herokuapp.com',
   version: '1.0.0',
 };
 
-// export const checkMode = {
-//   mode: (environment.apiBaseUrl.indexOf("dev") > 0) ? "dev" :
-//     (environment.apiBaseUrl.indexOf("uat") > 0) ? "uat" : "prod",
-//   sso: (environment.apiBaseUrl.indexOf("dev") > 0) ? "https://myreports.dev.phototype.com" :
-//       (environment.apiBaseUrl.indexOf("uat") > 0) ? "https://myreports.uat.phototype.com" :
-//         (environment.apiBaseUrl.indexOf("local") > 0) ? "http://10.10.22.161:4400" :
-//           "https://myreports.phototype.com"
-// }
+
 
 
 
